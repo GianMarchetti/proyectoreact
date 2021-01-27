e3f39828fd1cd396e88e51aa8c99cbfb40486fe6
Gian Marchetti <gian11marchetti@gmail.com>
2021-01-27 14:49:02 -0300

    Iniciando el Proyecto de React

---
+++ src/components/navbar/index.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootsrap.min.css';
+
+const NavbarComponent = () => {
+    return ( <
+        nav >
+        <
+        img src = "/images/arrow2189.png" / >
+        <
+        H1 > Indumentaria MM < /H1> <
+        a href = "" > Inicio < /a> <
+        a href = "" > Productos < /a> <
+        a href = "" > Carrito < /a> <
+        a href = "" > Contacto < /a> <
+        /nav>
+    );
+}
+
+export default NavbarComponent
