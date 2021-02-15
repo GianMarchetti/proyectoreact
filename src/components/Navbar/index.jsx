0c60c1294f71fb1887ba17e71b42e991bc3f0049
Gian Marchetti <gian11marchetti@gmail.com>
2021-02-15 19:37:34 -0300

    Primera entrega Proyecto Final

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,12 +2,13 @@
 //import React from 'react';
 import CartWidget from "../CartWidget";
 import {nav} from "./navbar.module.css";
+import { Link } from "react-router-dom";
 
 let imnav = { height:200, width:200, }
 const NavbarComponent = () => {
     return ( 
         <nav className={nav}>
-            <img src="https://i.ibb.co/HKCm9z0/logodbz.jpg" alt="icon e-commerce" style={imnav} border="0"/>
+            <Link to={`/`} ><img src="https://i.ibb.co/HKCm9z0/logodbz.jpg" alt="icon e-commerce" style={imnav} border="0"/></Link>
             <h1 > Indumentaria TenkaiKicks </h1> 
             <CartWidget />
         </nav>
